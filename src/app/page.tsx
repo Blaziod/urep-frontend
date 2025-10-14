78cf85a56d1a400553244af056400bafff8ee261
Nduka Ugochukwu <62814725+thatzerroguy@users.noreply.github.com>
2025-10-14 13:39:19 +0100

    feat: added other programs section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ import FooterBar from "@/components/FooterBar";
 export default function Home() {
   return (
       <div className={'pt-1'}>
-        <div className={'pt-1 px-4 sm:px-8 md:px-16 lg:px-32'}>
-          <div className={'mx-auto flex flex-col sm:flex-row items-center px-1 py-2 bg-[#277B12] mb-4'}>
+        <div className={'px-4 sm:px-8 md:px-16 lg:px-1'}>
+          <div className={'mx-auto flex flex-col sm:flex-row items-center px-1 py-2 bg-[#277B12]'}>
             <div className="flex items-center">
               <Image className={'mr-2'} src={'/images/fmyd_logo.png'} alt={'logo'} width={80} height={80} />
               <h1 className={'text-white text-xl sm:text-2xl font-bold'}>Yopi Tracker</h1>
             </div>
-            <div className={'grow my-4 sm:my-0'}>
+            <div className={'grow sm:my-0'}>
               <div className={'flex flex-wrap items-center justify-center gap-2 md:gap-4 lg:gap-8'}>
                 <Link className={'text-white text-sm sm:text-base md:text-lg'} href= '/'>Home</Link>
                 <Link className={'text-white text-sm sm:text-base md:text-lg'} href= '/'>About</Link>
@@ -32,8 +32,8 @@ export default function Home() {
           </div>
 
           <div className={'flex flex-col items-center w-full'}>
-            <div className={'flex flex-col md:flex-row justify-center items-center gap-6 md:gap-x-8 lg:gap-x-16 mt-6 md:mt-10'}>
-              <Image src={'/images/man1.png'} alt={'man'} width={250} height={250} className="hidden md:block" />
+            <div className={'flex flex-col md:flex-row justify-center items-center gap-6 md:gap-x-8 lg:gap-x-16'}>
+              <Image src={'/images/man1.png'} alt={'man'} width={300} height={300} className="hidden md:block" />
 
               <div className={'flex flex-col items-center gap-y-2 text-center px-4'}>
                 <h1 className={'text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-[#277B12]'}>FMYD</h1>
@@ -42,10 +42,10 @@ export default function Home() {
                 <h4 className={'text-black text-base sm:text-lg md:text-xl text-center'}>REALTIME YOUTH POLICY MONITORING AND EMPOWERMENT PROGRAMS</h4>
               </div>
 
-              <Image src={'/images/man2.png'} alt={'man'} width={250} height={250} className="hidden md:block" />
+              <Image src={'/images/man2.png'} alt={'man'} width={300} height={300} className="hidden md:block" />
             </div>
-            <div className={'flex flex-row space-x-4 mt-6 md:mt-10 justify-between w-full max-w-xs md:max-w-md'}>
-              <div className={'w-32 h-12 bg-[#277B12] rounded-lg flex flex-row justify-evenly items-center'}>
+            <div className={'flex flex-row space-x-4 mt-6 md:mt-10 justify-between w-full px-8'}>
+              <div className={'w-44 h-12 bg-[#277B12] rounded-lg flex flex-row justify-evenly items-center'}>
                 <FaSearch color={'white'} size={18}></FaSearch>
                 <p className={'text-white text-center font-medium'}>Search</p>
               </div>
@@ -69,7 +69,7 @@ export default function Home() {
           {/*Program specific sections*/}
           {/* Bakeprenuer Nigeria */}
           {/* TODO: Make section like a carousel, with a button to scroll to next/prev program. Each program should have a title, description, image, and a link to the program page */}
-          <div className={'flex flex-col justify-center w-full bg-gradient-to-b from-[#277B12] to-[#F9E79F] mt-6 md:mt-10 py-6 px-4'}>
+          <div className={'flex flex-col justify-center w-full bg-gradient-to-b from-[#277B12] to-[#F9E79F] my-6 md:mt-10 py-6 px-4'}>
             <h2 className={'font-semibold text-2xl sm:text-3xl md:text-4xl text-center text-white mb-4 md:mb-6'}>Bakeprenuer Nigeria</h2>
             <div className={'flex flex-col lg:flex-row items-center gap-6 lg:gap-x-10 xl:gap-x-20'}>
               <div className={'flex-col justify-start w-full lg:w-1/2'}>
@@ -83,17 +83,102 @@ export default function Home() {
                 <hr className="my-3"/>
                 <h1 className={'text-white text-2xl md:text-3xl lg:text-4xl font-semibold'}>Objective</h1>
                 <ul className="pl-4">
-                  <li className={'text-gray-500 font-normal text-base md:text-lg text-wrap leading-6 md:leading-8'}>To provide youth with modern baking and confectionery skills to enable them become self-reliant and employers of labour.</li>
-                  <li className={'text-gray-500 font-normal text-base md:text-lg text-wrap leading-6 md:leading-8'}>To empower youth to establish small and medium scale enterprises in the baking and confectionery industry.</li>
-                  <li className={'text-gray-500 font-normal text-base md:text-lg text-wrap leading-6 md:leading-8'}>To reduce youth unemployment and restiveness in the country.</li>
-                  <li className={'text-gray-500 font-normal text-base md:text-lg text-wrap leading-6 md:leading-8'}>To promote the culture of entrepreneurship among the youth.</li>
+                  <li className={'text-gray-500 font-normal text-base md:text-lg text-wrap leading-6 md:leading-8'}>To provide Nigerian youths with modern baking and confectionery skills aligned with industry standards.</li>
+                  <li className={'text-gray-500 font-normal text-base md:text-lg text-wrap leading-6 md:leading-8'}>To promote entrepreneurship and self-reliance through practical, hands-on learning.</li>
                 </ul>
                 <h3 className={'justify-self-end text-lg md:text-xl text-white font-semibold'}>Learn More</h3>
                 <hr className="my-3"/>
+                <h1 className={'text-white text-2xl md:text-3xl lg:text-4xl font-semibold'}>Who Should Attend</h1>
+                <ul className="pl-4">
+                  <li className={'text-gray-500 font-normal text-base md:text-lg text-wrap leading-6 md:leading-8'}>Unemployed and underemployed youths aged 18–35 years.</li>
+                  <li className={'text-gray-500 font-normal text-base md:text-lg text-wrap leading-6 md:leading-8'}>Young school leavers and graduates seeking practical livelihood skills.</li>
+                </ul>
+                <h3 className={'justify-self-end text-lg md:text-xl text-white font-semibold'}>Learn More</h3>
               </div>
               <div className={'flex flex-col justify-center items-center w-full lg:w-1/2'}>
                 <Image src={'/images/cooking_chefs.png'} alt={'Bakeprenuer'} width={900} height={500} className={'rounded-lg w-full max-w-2xl'}/>
-                <h3 className={'text-center text-xl md:text-2xl text-[#277B12] font-semibold mt-3'}>Register Now</h3>
+                <h3 className={'text-center text-xl md:text-xl text-[#277B12] font-semibold mt-7'}>Register Now</h3>
+              </div>
+            </div>
+          </div>
+
+            {/* Other Programs Section */}
+          <div className={'flex flex-col w-full items-center justify-center px-8'}>
+            <h3 className={'text-center text-xl md:text-xl text-[#277B12] font-semibold my-7'}>Other Programs</h3>
+
+            <div className={'w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-16'}>
+              <div className={'h-[250px] px-4 py-4 flex flex-col justify-center items-center shadow-2xl rounded-md'}>
+                <p className="text-center mb-4">LOCAL YOUTH EXCHANGE PROGRAMME</p>
+                <div className={'flex items-center justify-center rounded-lg bg-[#277B12] py-1 w-20'}>
+                  <p className={'text-white text-sm text-center'}>Read More</p>
+                </div>
+              </div>
+              <div className={'h-[250px] px-4 py-4 flex flex-col justify-center items-center shadow-2xl rounded-md'}>
+                <p className="text-center mb-4">TRAINING AND EMPOWERMENT OF
+                  YOUTH ON WASTE TO WEALTH
+                  MANAGEMENT, RECYCLING
+                  AND REUSE IN EACH OF
+                  THE 6 GEO-POLITICAL ZONES</p>
+                <div className={'flex items-center justify-center rounded-lg bg-[#277B12] py-1 w-20'}>
+                  <p className={'text-white text-sm text-center'}>Read More</p>
+                </div>
+              </div>
+              <div className={'h-[250px] px-4 py-4 flex flex-col justify-center items-center shadow-2xl rounded-md'}>
+                <p className="text-center mb-4">YOUTH MENTAL HEALTH AND
+                  PSYCHOSOCIAL SUPPORT (MHPS)
+                  PROGRAMME NATION-WIDE</p>
+                <div className={'flex items-center justify-center rounded-lg bg-[#277B12] py-1 w-20'}>
+                  <p className={'text-white text-sm text-center'}>Read More</p>
+                </div>
+              </div>
+              <div className={'h-[250px] px-4 py-4 flex flex-col justify-center items-center shadow-2xl rounded-md'}>
+                <p className="text-center mb-4">REVIEW AND IMPLEMENTATION
+                  OF NIGERIA YOUTH EMPLOYMENT
+                  ACTION PLAN (NIYEAP)</p>
+                <div className={'flex items-center justify-center rounded-lg bg-[#277B12] py-1 w-20'}>
+                  <p className={'text-white text-sm text-center'}>Read More</p>
+                </div>
+              </div>
+              <div className={'h-[250px] px-4 py-4 flex flex-col justify-center items-center shadow-2xl rounded-md'}>
+                <p className="text-center mb-4">ESTABLISHMENT OF TECH/SOCIAL
+                  HUBS IN THE NATIONAL YOUTH
+                  DEVELOPMENT CENTRES
+                  IN TWO ZONES SOUTH AND NORTH</p>
+                <div className={'flex items-center justify-center rounded-lg bg-[#277B12] py-1 w-20'}>
+                  <p className={'text-white text-sm text-center'}>Read More</p>
+                </div>
+              </div>
+              <div className={'h-[250px] px-4 py-4 flex flex-col justify-center items-center shadow-2xl rounded-md'}>
+                <p className="text-center mb-4">YOUTH ENTREPRENEURSHIP
+                  DEVELOPMENT PROGRAM
+                  FOR SUSTAINABLE GROWTH</p>
+                <div className={'flex items-center justify-center rounded-lg bg-[#277B12] py-1 w-20'}>
+                  <p className={'text-white text-sm text-center'}>Read More</p>
+                </div>
+              </div>
+              <div className={'h-[250px] px-4 py-4 flex flex-col justify-center items-center shadow-2xl rounded-md'}>
+                <p className="text-center mb-4">DIGITAL SKILLS TRAINING
+                  FOR RURAL YOUTH
+                  EMPOWERMENT</p>
+                <div className={'flex items-center justify-center rounded-lg bg-[#277B12] py-1 w-20'}>
+                  <p className={'text-white text-sm text-center'}>Read More</p>
+                </div>
+              </div>
+              <div className={'h-[250px] px-4 py-4 flex flex-col justify-center items-center shadow-2xl rounded-md'}>
+                <p className="text-center mb-4">AGRICULTURAL INNOVATION
+                  AND MODERNIZATION
+                  FOR YOUTH FARMERS</p>
+                <div className={'flex items-center justify-center rounded-lg bg-[#277B12] py-1 w-20'}>
+                  <p className={'text-white text-sm text-center'}>Read More</p>
+                </div>
+              </div>
+              <div className={'h-[250px] px-4 py-4 flex flex-col justify-center items-center shadow-2xl rounded-md'}>
+                <p className="text-center mb-4">CREATIVE ARTS AND
+                  CULTURAL HERITAGE
+                  PRESERVATION INITIATIVE</p>
+                <div className={'flex items-center justify-center rounded-lg bg-[#277B12] py-1 w-20'}>
+                  <p className={'text-white text-sm text-center'}>Read More</p>
+                </div>
               </div>
             </div>
           </div>
